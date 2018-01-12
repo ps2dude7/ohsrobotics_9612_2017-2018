9976cf59caf2d87c652ac255dabab85baeb8ed89
Unknown <weatherfreak10@gmail.com>
2018-01-12 11:24:28 -0600

    oh noes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ugandanBoiOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ugandanBoiOp.java
@@ -13,7 +13,7 @@ public class ugandanBoiOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         float left;
         float right;
-        float clawGrip;
+        float clawGrip = 0;
         boolean winch = false;
 
         hardware.init(hardwareMap);
@@ -24,34 +24,33 @@ public class ugandanBoiOp extends LinearOpMode {
 
             left = gamepad1.left_stick_y;
             right = -(gamepad1.right_stick_y);
-            clawGrip = gamepad1.right_trigger;
-            clawUngrip = -(gamepad1.left_trigger);
+
 
             //drive
             hardware.leftDrive.setPower(left);
             hardware.leftDrive.setPower(right);
 
-            //
-            hardware.clawServo1.setPosition(180);
-            hardware.clawServo2.setPosition(180);
-            hardware.clawServo3.setPosition(180);
-            hardware.clawServo4.setPosition(180);
+            //servos
+            hardware.clawServo1.setPosition(clawGrip);
+            hardware.clawServo2.setPosition(clawGrip);
+            hardware.clawServo3.setPosition(clawGrip);
+            hardware.clawServo4.setPosition(clawGrip);
 
             //EMBRACE THE JANK
             if (gamepad1.right_bumper) {
                 hardware.armWinch.setPower(1);
             } else if (gamepad1.left_bumper) {
                 hardware.armWinch.setPower(-1);
-            } else if (gamepad1.right_trigger <= 0.05); {
+            } else if (gamepad1.right_trigger <= 0.05) {
                 hardware.clawServo1.setPosition(clawGrip++);
                 hardware.clawServo2.setPosition(clawGrip++);
                 hardware.clawServo3.setPosition(-(clawGrip++));
                 hardware.clawServo4.setPosition(-(clawGrip++));
             } else if (gamepad1.left_trigger <= 0.05) {
-                hardware.clawServo1.setPosition(-(clawGrip));
-                hardware.clawServo2.setPosition(-(clawGrip));
-                hardware.clawServo3.setPosition(clawGrip);
-                hardware.clawServo4.setPosition(clawGrip);
+                hardware.clawServo1.setPosition(-(clawGrip++));
+                hardware.clawServo2.setPosition(-(clawGrip++));
+                hardware.clawServo3.setPosition(clawGrip++);
+                hardware.clawServo4.setPosition(clawGrip++);
             }
         }
     }
