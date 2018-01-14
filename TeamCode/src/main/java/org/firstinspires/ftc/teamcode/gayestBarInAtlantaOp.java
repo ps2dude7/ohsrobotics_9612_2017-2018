d57de76e0a53caf4df0581381a3ab344364dc810
Garrett Summerfield <weatherfreak10@gmail.com>
2018-01-13 19:27:04 -0600

    Revert "we broke something but for logging"
    This reverts commit 5322656145ac3981240e4cc31f846be4a97f159b.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gayestBarInAtlantaOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gayestBarInAtlantaOp.java
@@ -13,11 +13,11 @@ public class gayestBarInAtlantaOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         float left;
         float right;
-
-        float clawGrip1     = 140;
-        float clawGrip2     = 140;
-        float clawGrip3     = 40;
-        float clawGrip4     = 40;
+        float clawGrip1 = 0;
+        float clawGrip2 = 0;
+        float clawGrip3 = 0;
+        float clawGrip4 = 0;
+        boolean winch = false;
 
         hardware.init(hardwareMap);
 
@@ -34,8 +34,6 @@ public class gayestBarInAtlantaOp extends LinearOpMode {
             hardware.rightDrive.setPower(right);
 
             //EMBRACE THE JANK
-
-            //winch
             if (gamepad2.right_bumper) {
                 hardware.armWinch.setPower(1);
             } else if (gamepad2.left_bumper) {
@@ -44,60 +42,21 @@ public class gayestBarInAtlantaOp extends LinearOpMode {
                 hardware.armWinch.setPower(0);
             }
 
-            //claws
-            if (gamepad2.right_trigger >= 0.
-                clawGrip1+=.1;
-                clawGrip2+=.1;
-                clawGrip3+=.1;
-                clawGrip4+=.1;
-                hardware.clawServo1.setPosition(clawGrip1);
-                hardware.clawServo2.setPosition(clawGrip2);
-                hardware.clawServo3.setPosition(clawGrip3);
-                hardware.clawServo4.setPosition(clawGrip4);
-
-
-            } else if (gamepad2.left_trigger >= 0.1) {
-                clawGrip1-=.1;
-                clawGrip2-=.1;
-                clawGrip3-=.1;
-                clawGrip4-=.1;
-                hardware.clawServo1.setPosition(clawGrip1);
-                hardware.clawServo2.setPosition(clawGrip2);
-                hardware.clawServo3.setPosition(clawGrip3);
-                hardware.clawServo4.setPosition(clawGrip4);
-            }
-
-            if (clawGrip1 < 40) {
-                clawGrip1 = 40;
-                clawGrip2 = 40;
-            } else if (clawGrip1 > 140) {
-                clawGrip1 = 140;
-                clawGrip2 = 140;
-            }
-
-            if (clawGrip3 < 40) {
-                clawGrip3 = 40;
-                clawGrip4 = 40;
-            } else if (clawGrip3 > 140) {
-                clawGrip3 = 140;
-                clawGrip4 = 140;
-            }
-            //relic motor
-            if (gamepad1.right_bumper) {
-                hardware.relicMotor.setPower(0.07);
-            } else if (gamepad1.left_bumper) {
-                hardware.relicMotor.setPower(-0.2);
+            if (gamepad2.right_trigger <= 0.1) {
+                hardware.clawServo1.setPosition(clawGrip1++);
+                hardware.clawServo2.setPosition(clawGrip2++);
+                hardware.clawServo3.setPosition(-(clawGrip3++));
+                hardware.clawServo4.setPosition(-(clawGrip4++));
+            } else if (gamepad2.left_trigger <= 0.1) {
+                hardware.clawServo1.setPosition(-(clawGrip1++));
+                hardware.clawServo2.setPosition(-(clawGrip2++));
+                hardware.clawServo3.setPosition(clawGrip3++);
+                hardware.clawServo4.setPosition(clawGrip4++);
             } else {
-                hardware.relicMotor.setPower(0);
-            }
-
-            //relic claw
-            if (gamepad1.right_trigger >= 0.1) {
-                hardware.relicServo1.setPower(-1);
-            } else if (gamepad1.left_trigger >= 0.1) {
-                hardware.relicServo1.setPower(1);
-            } else if (gamepad1.right_trigger <= 0.1 && gamepad1.left_trigger <= 0.1) {
-                hardware.relicServo1.setPower(0);
+                hardware.clawServo1.setPosition(clawGrip1);
+                hardware.clawServo2.setPosition(clawGrip2);
+                hardware.clawServo3.setPosition(clawGrip3);
+                hardware.clawServo4.setPosition(clawGrip4);
             }
         }
     }
